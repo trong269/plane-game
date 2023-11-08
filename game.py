61879ea02e3a390c9ef07fe2da5fe31aeff836e6
trong269 <trongbg2692004@gmail.com>
2023-11-08 17:26:33 +0700

    third commit

--- game.py
+++ game.py
@@ -6,7 +6,7 @@ def create_rocket():
     rocket_rect = rocket_surface.get_rect( center = ( rocket_wight , 0 ) )
     return rocket_rect
 def draw_rocket( rocket_list ):
-    print( len( rocket_list ) )
+    print( len(rocket_list) )
     for rocket in rocket_list:
         Screen.blit( rocket_surface , rocket )
 def move_rocket( rocket_list ):
@@ -15,15 +15,13 @@ def move_rocket( rocket_list ):
             rocket_rect.centery += 3
         else:
             rocket_list.remove( rocket_rect)
-    return rocket_list 
 def creat_bullet():
-    bullet_rect = bullet_surface.get_rect( center = ( 0 , 740) )
+    bullet_rect = bullet_surface.get_rect( center = ( plane_rect.centerx , plane_rect.centery ) )
     return bullet_rect
-def move_bullet( centerx , bullet_list ):
+def move_bullet( bullet_list ):
     for bullet_rect in bullet_list :
         if bullet_rect.centery >= -20 :
             bullet_rect.centery -= 5 
-            bullet_rect.centerx = centerx
         else :
             bullet_list.remove( bullet_rect )
 def draw_bullet( bullet_list ):
@@ -34,6 +32,12 @@ def check_die( bullet_list ):
         if plane_rect.colliderect( bullet_rect ):
             return False
     return True
+def headshot( bullet_list , rocket_list ):
+    for bullet in bullet_list :
+        for rocket in rocket_list :
+            if bullet.colliderect( rocket ):
+                bullet_list.remove( bullet )
+                rocket_list.remove( rocket )
 #set up
 Screen = pygame.display.set_mode ( (938 ,780) )
 clock = pygame.time.Clock()
@@ -46,14 +50,18 @@ plane_movementx = 0
 plane_movementy = 0 
 #tao rocket
 rocket_surface = pygame.image.load( r"Images\Objects\ship_blue.png")
-rocket_event = pygame.USEREVENT + 1
-pygame.time.set_timer( rocket_event , 1000 )
+rocket_speed_event = pygame.USEREVENT + 1
+pygame.time.set_timer( rocket_speed_event , 5000 ) # cu moi 5s thi tang so luong rocket
+rocket_event = pygame.USEREVENT + 2
+rocket_speed = 1000
+pygame.time.set_timer( rocket_event , rocket_speed )
 rocket_list = []
-rocket_pos = [ 10 , 100 , 200 , 300 , 400 , 500 , 600 , 700 , 800 , 900 ]
+rocket_pos = [ 15 , 100 , 200 , 300 , 400 , 500 , 600 , 700 , 800 , 900 ]
 # tao bullet cho plane
 bullet_surface = pygame.image.load( r"Images\Objects\bullet4.png")
-bullet_event = pygame.USEREVENT + 2
-pygame.time.set_timer( bullet_event , 200 )
+bullet_speed = 200 
+bullet_event = pygame.USEREVENT + 3
+pygame.time.set_timer( bullet_event , bullet_speed )
 bullet_list = [ ]
 # check collision giua rocket va plane
 Active = True 
@@ -76,6 +84,7 @@ while True:
             Active = True 
             rocket_list.clear()
             bullet_list.clear()
+            rocket_speed = 1000 
             plane_rect = plane_surface.get_rect( midbottom = ( 450 , 750 ) )
         if event.type == pygame.KEYUP:
             plane_movementx = 0 
@@ -84,18 +93,22 @@ while True:
             rocket_list.append( create_rocket() )
         if event.type == bullet_event:
             bullet_list.append( creat_bullet() )
+        if event.type == rocket_speed_event and rocket_speed >= 250 :
+            rocket_speed -= 50
+            pygame.time.set_timer( rocket_event , rocket_speed )
     Screen.blit( bg , ( 0 , 0 ) )
     if Active:
         #plane
-        move_bullet( plane_rect.centerx, bullet_list )
+        move_bullet( bullet_list )
         draw_bullet( bullet_list )
         Screen.blit( plane_surface , plane_rect )
         plane_rect.centerx += plane_movementx
         plane_rect.centery += plane_movementy
         #rocket
-        rocket_list = move_rocket( rocket_list )
-        draw_rocket(rocket_list)
+        move_rocket( rocket_list )
+        draw_rocket(rocket_list )
+        headshot(bullet_list , rocket_list)
         Active = check_die( rocket_list )
     
     pygame.display.update()
-    clock.tick( 60 )
+    clock.tick( 80 )
